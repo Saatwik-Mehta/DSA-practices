214cf1ba2042710ccc4712c0de60286ea3e05f8f
Saatwik-Mehta <80687021+Saatwik-Mehta@users.noreply.github.com>
2025-08-27 15:41:25 +0000

    first algorithm for two sum

---
+++ two_pointers_unsorted.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # - don't return same element twice - means the number on the same index x and y
+        #   pointing to the same index.
+        # - exactly one solution for each target
+        # - return in any order
+
+        # Optimised solution:
+        # start i from index 0 and j from end index of the array,
+        # create a list of tuple of index and number, that stores the original index of the number for the final result.
+        # sort the newly created list accoding to the number key in ascending, so that two-pointer approach can be applied.
+        # start a while loop with condition i < j,
+        # compare the sum with the target if it's equal return
+        # if the sum is smaller, increment i and retry,
+        # if the sum is greater, decrement j and retry.
+
+        i = 0
+        j = len(nums) - 1
+        # O(n) -> for space complexity for new array being created
+        nums = [(index, number) for index, number in enumerate(nums)]
+
+        # O(n log n) -> time complexity for sorting operation
+        nums.sort(key= lambda x: x[1])
+        print(nums)
+        while i < j:
+            if nums[i][1] + nums[j][1] == target:
+                return list((nums[i][0], nums[j][0]))
+            elif nums[i][1] + nums[j][1] > target:
+                j -= 1
+            else: i += 1
+        return "result not found"
