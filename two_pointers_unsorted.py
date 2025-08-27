94c1196f58464eacfc4d8cfe36f47545ec9e0a6f
Saatwik-Mehta <80687021+Saatwik-Mehta@users.noreply.github.com>
2025-08-27 17:13:40 +0000

    solution two with steps

--- two_pointers_unsorted.py
+++ two_pointers_unsorted.py
@@ -2,7 +2,9 @@ from typing import List
 
 class Solution:
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # Time Complexity: O(n log n) -> Sorting operation (TIM SORT)
+    # Space Complexity: O(n) -> New array being created
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
         # - don't return same element twice - means the number on the same index x and y
         #   pointing to the same index.
         # - exactly one solution for each target
@@ -32,3 +34,36 @@ class Solution:
                 j -= 1
             else: i += 1
         return "result not found"
+    
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        # - don't return same element twice - means the number on the same index x and y
+        #   pointing to the same index.
+        # - exactly one solution for each target
+        # - return in any order
+
+        # Optimised solution:
+        # create a hash that registers unique item and it's index as the value
+        # start a pointer for the iterating
+
+        # Now, run a while loop from 0 to len(nums)-1,
+        # and check for the other number, by subtracting the number at index i from target,
+        # now, if other number already exists in the hash, return the value(index of the other number)
+        #  from the hash as well as the i index
+
+        # otherwise store the number at ith index in the hash,
+        # which will be checked as the other number in the next iternation
+
+        check_other_number = {}
+        i = 0
+
+        while i < len(nums):
+            y = target - nums[i]
+            if y in check_other_number:
+                return list((check_other_number[y], i))
+            check_other_number[nums[i]] = i
+            i+=1
+        return "result not found"
+        
+    
